8f25d49719a5da855a46381c0e9521f6a9d38977
Chun Rong <crphang@gmail.com>
2016-02-22 21:16:45 +0800

    Add more meat

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,36 @@
-public class Controller {
+import java.util.Scanner;
 
+public class Controller {
+	
+	private static Scanner _scanner;
+	
 	public static void main(String[] args) {
+		String nameOfFile = args[0];
 		
+//		Create or set file
+		createOrFindFile(nameOfFile);
+		
+//		Initialize state with existing or new file (Actions)
+				
+		repl();
+	}
+	
+	public static void createOrFindFile(String nameOfFile) {
+		
+	}
+	
+	private static void repl() {
+	    _scanner = new Scanner(System.in);
+
+	    while(true) {
+	    	String userCommand = _scanner.nextLine();
+
+//	    	Parse Tokens
+	    	
+//	    	Do Actions
+	    	
+//	    	Render View
+	    }
 	}
 
 }
