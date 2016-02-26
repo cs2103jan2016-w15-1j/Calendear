a76a1a139567139e834d6e2787d0859f4a08820d
Pan Jiyun <panjiyun@Pan-2.local>
2016-02-26 16:54:16 +0800

    update view 2

--- Calendear/src/calendear/view/View.java
+++ Calendear/src/calendear/view/View.java
@@ -1,9 +1,13 @@
 package calendear.view;
 
+import java.util.ArrayList;
+
+import calendear.util.Task;
+
 public class View {
 	
 	private static final String HEADER_NAME = "task name: ";
-	private static final String HEADER_TYPE = "type:\n";
+	private static final String HEADER_TAG = "tag:\n";
 	private static final String HEADER_STARTTIME = "start time: ";
 	private static final String HEADER_ENDTIME = "due time: ";
 	private static final String HEADER_RECURRING_ENDTIME = "next due time:  ";
@@ -28,8 +32,50 @@ public class View {
 	
 	private static final String MSG_WELCOME = "welcome to calendear!";
 	private static final String MSG_COMMAND = "command:";
+	private static final String MSG_YES = "yes";
+	private static final String MSG_NO = "no";
 	
+	public static String getDetailsOfTaskArr(ArrayList<Task> taskArr){
+		String output="";
+		for(int i=0;i<taskArr.size();i++){
+			if(taskArr.get(i)!=null){
+				output+=i+".\n";
+				output+=getDetailsOfTask(taskArr.get(i))+"\n";
+			}
+		}
+		return output;
+	}
 	
+	public static String getDetailsOfTask(Task task){
+		String details="";
+		if(task.getName()!=null){
+			details+=HEADER_NAME+task.getName()+"\n";
+		}
+		if(task.getTag()!=null){
+			details+=HEADER_TAG+task.getTag()+"\n";
+		}	
+		if(task.getStartTime()!=null){
+			details+=HEADER_STARTTIME+task.getStartTimeStr()+"\n";
+		}
+		if(task.getEndTime()!=null){
+			details+=HEADER_STARTTIME+task.getEndTimeStr()+"\n";
+		}
+		if(task.getLocation()!=null){
+			details+=HEADER_LOCATION+task.getLocation()+"\n";
+		}
+		if(task.getNote()!=null){
+			details+=HEADER_NOTE+task.getNote()+"\n";
+		}
+		if(task.isImportant()){
+			details+=HEADER_IMPORTANCE+ MSG_YES+ "\n";
+		}
+		if(task.isFinished()){
+			details+=HEADER_FINISHED+MSG_YES+"\n";
+		}
+		else if(!task.isFinished()){
+			details+=HEADER_FINISHED+MSG_NO+"\n";
+		}
+	}
 
 	
 	
