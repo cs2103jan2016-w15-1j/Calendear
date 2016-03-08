e1379c7c2dc2b5a1b04e5d32151292b1c05793be
Pan Jiyun <panjiyun@r-74-106-25-172.comp.nus.edu.sg>
2016-03-08 13:33:53 +0800

    added parseUpdateCmd2 in Parser and edited CommandUpdate

--- src/calendear/util/CommandUpdate.java
+++ src/calendear/util/CommandUpdate.java
@@ -11,18 +11,37 @@ public class CommandUpdate extends Command {
 	private int index;
 	private String newName;
 	
+	private int[] updateChecklist;
+	private String[] newInfo;
+	
+	
+			
 	public CommandUpdate (int index, String newName){
 		type = CMD_TYPE.UPDATE;
 		this.index = index;
 		this.newName = newName;
 	}
 	
+	public CommandUpdate (int index, int[] updateChecklist, String[] newInfo){
+		type = CMD_TYPE.UPDATE;
+		this.updateChecklist = updateChecklist;
+		this.newInfo = newInfo;
+	}
+	
 	public int getIndex(){
 		return index;
 	}
+	
 	
 	public String getNewName(){
 		return newName;
 	}
 	
+	public int[] getChecklist(){
+		return updateChecklist;
+	}
+	
+	public String[] getNewInfo(){
+		return newInfo;
+	}
 }
