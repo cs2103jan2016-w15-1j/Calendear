9d8fc743a0bd816f4f69e56292bdb98593bf4532
vietthang207 <vietthang207@gmail.com>
2016-03-09 14:25:14 +0800

    fix bug in CommandUpdate constructor

--- src/calendear/util/CommandUpdate.java
+++ src/calendear/util/CommandUpdate.java
@@ -33,6 +33,7 @@ public class CommandUpdate extends Command {
 	
 	public CommandUpdate (int index, boolean[] updateChecklist, Object[] newInfo){
 		type = CMD_TYPE.UPDATE;
+		this.index = index;
 		this.updateChecklist = updateChecklist;
 		this.newInfo = newInfo;
 	}
