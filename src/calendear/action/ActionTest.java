d4d40244a6708295bba36419b9a9385feb384957
a0129998 <a0129998@u.nus.edu>
2016-03-18 21:36:45 +0800

    added junit test for exeAdd

--- src/calendear/action/ActionTest.java
+++ src/calendear/action/ActionTest.java
@@ -3,16 +3,29 @@ package calendear.action;
 import static org.junit.Assert.*;
 
 import java.text.ParseException;
+import java.util.*;
 
 import org.junit.Test;
+
+import calendear.util.*;
 
 public class ActionTest {
 
 	@Test
-	public void test() throws ParseException {
-		fail("Not yet implemented");
-		Action a = new Action("save.txt");
-		
+	public void testUpdate() throws ParseException{
+		Action action1 = new Action("action1.txt");
+		Task t1 = new Task("task1", new GregorianCalendar(1,1,2001));
+		CommandAdd cA = new CommandAdd(t1);
+		action1.exeAdd(cA);
+		GregorianCalendar newTime = new GregorianCalendar(2, 1, 2001);
+		System.out.println(newTime.toString());
+		int index = 0;
+		boolean[] chklst = {false, false, false, true, false, false, false, false, false};
+		Object[] objArr = {null, null, null, (Object) newTime, null, null, null, null};
+		CommandUpdate cU = new CommandUpdate(index, chklst, objArr);
+		action1.exeUpdate(cU);
+		assertEquals(t1.getEndTime(), newTime);
 	}
+
 
 }
