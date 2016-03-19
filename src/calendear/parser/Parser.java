90532a1072ae69c2fe3f332639c12c9658b611c4
vietthang207 <vietthang207@gmail.com>
2016-03-19 15:44:24 +0800

    dots in keywords now completely removed

--- src/calendear/parser/Parser.java
+++ src/calendear/parser/Parser.java
@@ -1,6 +1,5 @@
 package calendear.parser;
 
-import java.util.GregorianCalendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import calendear.util.*;
@@ -21,7 +20,7 @@ public class Parser {
 	private static final String CMD_STR_TAG = "tag";
 	private static final String CMD_STR_LINK_GOOGLE = "linkGoogle";
 	private static final String CMD_STR_EXIT = "exit";
-	private static final String ESCAPE_CHARACTER = ".";
+	private static final String EMPTY = "";
 	//when using regex and regex-related methods like String.split() and String.replaceAll()
 	//the "." is treated as metacharacter so you have to include the escape character "\\"
 	private static final String PATTERN_ESCAPE_CHARACTER = "\\.";
@@ -38,49 +37,30 @@ public class Parser {
 	private static final String MARK = "mark";
 	private static final String DONE = "done";
 	
-	private static final String PATTERN_ADD_DEADLINE = 
-	"(\\badd) +(.+)(\\bby) +(.+)"
-	+ "(?:(?:(\\bat) *(.+)) *"			//represent the groups at and <newLocation>
-	+ "|(?:(\\bnote) *(.+)) *"			//represent the groups note and <newNote>
-	+ "|(?:(\\btag) *(.+)) *"			//represent the groups tag and <newTag>
-	+ "|(?:(\\bmark) *()) *"			//represent the group mark
-	+ "|(?:(\\bdone) *()) *)+";			//represent the group done
-	private static final String PATTERN_ADD_EVENT = 
-	"(\\badd) +(.+)(\\bfrom) +(.+)(\\bto)(.+)"
-	+ "|(?:(\\bat) *(.+)) *"			//represent the groups at and <newLocation>
-	+ "|(?:(\\bnote) *(.+)) *"			//represent the groups note and <newNote>
-	+ "|(?:(\\btag) *(.+)) *"			//represent the groups tag and <newTag>
-	+ "|(?:(\\bmark) *()) *"			//represent the group mark
-	+ "|(?:(\\bdone) *()) *)+";			//represent the group done
-	private static final String PATTERN_ADD_FLOATING = 
-	"(\\badd) +(.+)"
-	+ "|(?:(\\bat) *(.+)) *"			//represent the groups at and <newLocation>
-	+ "|(?:(\\bnote) *(.+)) *"			//represent the groups note and <newNote>
-	+ "|(?:(\\btag) *(.+)) *"			//represent the groups tag and <newTag>
-	+ "|(?:(\\bmark) *()) *"			//represent the group mark
-	+ "|(?:(\\bdone) *()) *)+";			//represent the group done;
 	private static final String PATTERN_ADD = 
-	"(\\badd) +([\\S]+) +"				//represent the groups update and <taskID>
-	+ "(?:(?:(\\bby) *(.+)) *"			//represent the groups by and <dateAndTime>
-	+ "|(?:(\\bfrom) *(.+)) *"			//represent the groups from and <dateAndTime>
-	+ "|(?:(\\bto) *(.+)) *"			//represent the groups to and <dateAndTime>
-	+ "|(?:(\\bfloat) *()) *"			//represent the group float
-	+ "|(?:(\\bat) *(.+)) *"			//represent the groups at and <newLocation>
-	+ "|(?:(\\bnote) *(.+)) *"			//represent the groups note and <newNote>
-	+ "|(?:(\\btag) *(.+)) *"			//represent the groups tag and <newTag>
-	+ "|(?:(\\bmark) *()) *)+";			//represent the group done
+	"(\\badd\\b) +(.+) +"				//represent the groups update and <taskID>
+	+ "(?:(?:(\\bname\\b) *(.+)) *"		//represent the groups name and <newName>
+	+ "|(?:(\\bby\\b) *(.+)) *"			//represent the groups by and <dateAndTime>
+	+ "|(?:(\\bfrom\\b) *(.+)) *"			//represent the groups from and <dateAndTime>
+	+ "|(?:(\\bto\\b) *(.+)) *"			//represent the groups to and <dateAndTime>
+	+ "|(?:(\\bfloat\\b) *()) *"			//represent the group float
+	+ "|(?:(\\bat\\b) *(.+)) *"			//represent the groups at and <newLocation>
+	+ "|(?:(\\bnote\\b) *(.+)) *"			//represent the groups note and <newNote>
+	+ "|(?:(\\btag\\b) *(.+)) *"			//represent the groups tag and <newTag>
+	+ "|(?:(\\bmark\\b) *()) *"			//represent the group mark
+	+ "|(?:(\\bdone\\b) *()) *)+";			//represent the group done
 	private static final String PATTERN_UPDATE_BY_INDEX = 
-	"(\\bupdate) +(\\d+) +"				//represent the groups update and <taskID>
-	+ "(?:(?:(\\bname) *(.+)) *"		//represent the groups name and <newName>
-	+ "|(?:(\\bby) *(.+)) *"			//represent the groups by and <dateAndTime>
-	+ "|(?:(\\bfrom) *(.+)) *"			//represent the groups from and <dateAndTime>
-	+ "|(?:(\\bto) *(.+)) *"			//represent the groups to and <dateAndTime>
-	+ "|(?:(\\bfloat) *()) *"			//represent the group float
-	+ "|(?:(\\bat) *(.+)) *"			//represent the groups at and <newLocation>
-	+ "|(?:(\\bnote) *(.+)) *"			//represent the groups note and <newNote>
-	+ "|(?:(\\btag) *(.+)) *"			//represent the groups tag and <newTag>
-	+ "|(?:(\\bmark) *()) *"			//represent the group mark
-	+ "|(?:(\\bdone) *()) *)+";			//represent the group done
+	"(\\bupdate\\b) +(\\d+) +"				//represent the groups update and <taskID>
+	+ "(?:(?:(\\bname\\b) *(.+)) *"		//represent the groups name and <newName>
+	+ "|(?:(\\bby\\b) *(.+)) *"			//represent the groups by and <dateAndTime>
+	+ "|(?:(\\bfrom\\b) *(.+)) *"			//represent the groups from and <dateAndTime>
+	+ "|(?:(\\bto\\b) *(.+)) *"			//represent the groups to and <dateAndTime>
+	+ "|(?:(\\bfloat\\b) *()) *"			//represent the group float
+	+ "|(?:(\\bat\\b) *(.+)) *"			//represent the groups at and <newLocation>
+	+ "|(?:(\\bnote\\b) *(.+)) *"			//represent the groups note and <newNote>
+	+ "|(?:(\\btag\\b) *(.+)) *"			//represent the groups tag and <newTag>
+	+ "|(?:(\\bmark\\b) *()) *"			//represent the group mark
+	+ "|(?:(\\bdone\\b) *()) *)+";			//represent the group done
 	
 	private static final int NUM_OF_UPDATE_KEYWORD = 10;
 	private static final int NUM_OF_TASK_ATTRIBUTES = 9;
@@ -124,66 +104,11 @@ public class Parser {
 	}
 	
 	private static Command parseAddCmd(String[] words, String rawInput){
-		
-		Pattern pattern = Pattern.compile(PATTERN_ADD_DEADLINE);
-		Matcher matcher = pattern.matcher(rawInput);
-		if (matcher.find()) {
-			return parseAddDeadline(rawInput, matcher);
-		}
-		
-		pattern = Pattern.compile(PATTERN_ADD_EVENT);
-		matcher = pattern.matcher(rawInput);
-		if (matcher.find()) {
-			return parseAddEvent(rawInput, matcher);
-		}
-		
-		pattern = Pattern.compile(PATTERN_ADD_FLOATING);
-		matcher = pattern.matcher(rawInput);
-		if (matcher.find()) {
-			return parseAddFloating(rawInput, matcher);
-		}
-		
-		return new CommandInvalid(rawInput);
-		
-	}
-	
-	// .add <name> .by dd/MM/yy HH:mm
-	private static Command parseAddDeadline(String rawInput, Matcher matcher) {
-		GregorianCalendar time;
-		try {
-			time = DateParser.parse(matcher.group(4));
-		} catch (ParseException e) {
-			return new CommandInvalid(e.getMessage());
-		}
-		Task task = new Task (matcher.group(2), time);
-		return new CommandAdd(task);
-	}
-	
-	// .add <name> .from dd/MM/yy HH:mm .to dd/MM/yy HH:mm
-	private static Command parseAddEvent(String rawInput, Matcher matcher) {
-		GregorianCalendar startTime, endTime;
-		try {
-			startTime = DateParser.parse(matcher.group(4));
-			endTime = DateParser.parse(matcher.group(6));
-		} catch (ParseException e) {
-			return new CommandInvalid(e.getMessage());
-		}
-		Task task = new Task (matcher.group(2), startTime, endTime);
-		return new CommandAdd(task);
-	}
-	
-	// .add <name>
-	private static Command parseAddFloating(String rawInput, Matcher matcher) {
-		Task task = new Task (matcher.group(2));
-		return new CommandAdd(task);
-	}
-	
-	private static Command parseAddCommand2(String[] words, String rawInput){
 		Pattern pattern = Pattern.compile(PATTERN_ADD);
 		Matcher matcher = pattern.matcher(rawInput);
 		if (matcher.find()){
 			try {	
-				String name = matcher.group(2);
+				String name = removeEscapeCharacter(matcher.group(2));
 				boolean[] checkList = new boolean[NUM_OF_TASK_ATTRIBUTES];
 				Object[] newInfo = new Object[NUM_OF_TASK_ATTRIBUTES];
 				makeCheckList(matcher, checkList, newInfo);
@@ -231,34 +156,39 @@ public class Parser {
 	private static void makeCheckList(Matcher matcher, boolean[] checkList, Object[] newInfo) 
 	throws ParseException {
 		for (int i=0; i<NUM_OF_UPDATE_KEYWORD; i++){
-			if (matcher.group(3 + 2*i)!=null){
+			if (matcher.group(3 + 2*i) != null){
 				/* for both int[]updateChesklist and String[]newInfo 
 				[0:name][1:type][2:starttime]
 				[3:endtime][4:location][5:note]
 				[6:tag][7:important][8:finished]
 				*/
+				String argument;
 				switch (matcher.group(3 + 2*i)){
 					case NAME:
 						checkList[CommandUpdate.CODE_UPDATE_NAME] = true;
-						newInfo[CommandUpdate.CODE_UPDATE_NAME] = matcher.group(4+2*i);
+						argument = removeEscapeCharacter(matcher.group(4+2*i));
+						newInfo[CommandUpdate.CODE_UPDATE_NAME] = argument;
 						break;
 					case BY:
 						checkList[CommandUpdate.CODE_UPDATE_TYPE] = true;
 						newInfo[CommandUpdate.CODE_UPDATE_TYPE] = TASK_TYPE.DEADLINE;
 						checkList[CommandUpdate.CODE_UPDATE_END_TIME] = true;
-						newInfo[CommandUpdate.CODE_UPDATE_END_TIME] = DateParser.parse(matcher.group(4+2*i));
+						argument = removeEscapeCharacter(matcher.group(4+2*i));
+						newInfo[CommandUpdate.CODE_UPDATE_END_TIME] = DateParser.parse(argument);
 						break;
 					case FROM:
 						checkList[CommandUpdate.CODE_UPDATE_TYPE] = true;
 						newInfo[CommandUpdate.CODE_UPDATE_TYPE] = TASK_TYPE.EVENT;
 						checkList[CommandUpdate.CODE_UPDATE_START_TIME] = true;
-						newInfo[CommandUpdate.CODE_UPDATE_START_TIME] = DateParser.parse(matcher.group(4+2*i));
+						argument = removeEscapeCharacter(matcher.group(4+2*i));
+						newInfo[CommandUpdate.CODE_UPDATE_START_TIME] = DateParser.parse(argument);
 						break;
 					case TO:
 						checkList[CommandUpdate.CODE_UPDATE_TYPE] = true;
 						newInfo[CommandUpdate.CODE_UPDATE_TYPE] = TASK_TYPE.EVENT;
 						checkList[CommandUpdate.CODE_UPDATE_END_TIME] = true;
-						newInfo[CommandUpdate.CODE_UPDATE_END_TIME] = DateParser.parse(matcher.group(4+2*i));
+						argument = removeEscapeCharacter(matcher.group(4+2*i));
+						newInfo[CommandUpdate.CODE_UPDATE_END_TIME] = DateParser.parse(argument);
 						break;
 					case FLOAT:
 						checkList[CommandUpdate.CODE_UPDATE_TYPE] = true;
@@ -266,15 +196,18 @@ public class Parser {
 						break;
 					case AT:
 						checkList[CommandUpdate.CODE_UPDATE_LOCATION] = true;
-						newInfo[CommandUpdate.CODE_UPDATE_LOCATION] = matcher.group(4+2*i);
+						argument = removeEscapeCharacter(matcher.group(4+2*i));
+						newInfo[CommandUpdate.CODE_UPDATE_LOCATION] = argument;
 						break;
 					case NOTE:
 						checkList[CommandUpdate.CODE_UPDATE_NOTE] = true;
-						newInfo[CommandUpdate.CODE_UPDATE_NOTE] = matcher.group(4+2*i);
+						argument = removeEscapeCharacter(matcher.group(4+2*i));
+						newInfo[CommandUpdate.CODE_UPDATE_NOTE] = argument;
 						break;
 					case TAG:
 						checkList[CommandUpdate.CODE_UPDATE_TAG] = true;
-						newInfo[CommandUpdate.CODE_UPDATE_TAG] = matcher.group(4+2*i);
+						argument = removeEscapeCharacter(matcher.group(4+2*i));
+						newInfo[CommandUpdate.CODE_UPDATE_TAG] = argument;
 						break;
 					case MARK:
 						checkList[CommandUpdate.CODE_UPDATE_IMPORTANT] = true;
@@ -288,72 +221,6 @@ public class Parser {
 			}
 		}
 	}
-		
-	/** 
-	 *  parseUpdateCmd2 by Jiyun
-	 * 
-	 **/
-	//.update <index> <header1:content\\r\\n header2:content\\r\\n ...>
-//	private static Command parseUpdateCmd2(String[] words, String rawInput){
-//			Pattern pattern = Pattern.compile(PATTERN_UPDATE_NAME_BY_INDEX);
-//			Matcher matcher = pattern.matcher(rawInput);
-//			if (matcher.find()){
-//				try {	
-//					int index = Integer.parseInt(matcher.group(2));
-//					String newInfo = matcher.group(3);
-//					int[] updateChecklist = new int[NUM_OF_TASK_ATTRIBUTES];
-//					String[] newInfoContentOnly = new String[NUM_OF_TASK_ATTRIBUTES];
-//					String[] newInfoArr = newInfo.split("\\r?\\n");
-//					for(int i=0;i<newInfoArr.length;i++){
-//						String[] line = newInfoArr[i].split(":");
-//						if(line[0].equalsIgnoreCase("name")){
-//							updateChecklist[0]=1;
-//							newInfoContentOnly[0]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("type")){
-//							updateChecklist[1]=1;
-//							newInfoContentOnly[1]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("starttime")){
-//							updateChecklist[2]=1;
-//							newInfoContentOnly[2]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("endtime")){
-//							updateChecklist[3]=1;
-//							newInfoContentOnly[3]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("location")){
-//							updateChecklist[4]=1;
-//							newInfoContentOnly[4]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("note")){
-//							updateChecklist[5]=1;
-//							newInfoContentOnly[5]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("tag")){
-//							updateChecklist[6]=1;
-//							newInfoContentOnly[6]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("important")){
-//							updateChecklist[7]=1;
-//							newInfoContentOnly[7]= line[1];
-//						}
-//						else if(line[0].equalsIgnoreCase("finished")){
-//							updateChecklist[8]=1;
-//							newInfoContentOnly[8]= line[1];
-//						}
-//					
-//						
-//					}
-//					return new CommandUpdate(index, updateChecklist, newInfoContentOnly);
-//				}
-//				catch (NumberFormatException e){
-//					return new CommandInvalid(rawInput);
-//				}
-//			}
-//			
-//			return new CommandInvalid(rawInput);
-//		}
 	
 	private static Command parseDeleteCmd(String[] words, String rawInput){
 		int index = Integer.parseInt(words[1]);
@@ -401,8 +268,73 @@ public class Parser {
 	}
 	
 	private static String removeEscapeCharacter(String rawInput){
-		return rawInput.replaceAll(PATTERN_ESCAPE_CHARACTER, rawInput);
+		return rawInput.replaceAll(PATTERN_ESCAPE_CHARACTER, EMPTY);
 	}
 	
 }
 
+/** 
+ *  parseUpdateCmd2 by Jiyun
+ * 
+ **/
+//.update <index> <header1:content\\r\\n header2:content\\r\\n ...>
+//private static Command parseUpdateCmd2(String[] words, String rawInput){
+//		Pattern pattern = Pattern.compile(PATTERN_UPDATE_NAME_BY_INDEX);
+//		Matcher matcher = pattern.matcher(rawInput);
+//		if (matcher.find()){
+//			try {	
+//				int index = Integer.parseInt(matcher.group(2));
+//				String newInfo = matcher.group(3);
+//				int[] updateChecklist = new int[NUM_OF_TASK_ATTRIBUTES];
+//				String[] newInfoContentOnly = new String[NUM_OF_TASK_ATTRIBUTES];
+//				String[] newInfoArr = newInfo.split("\\r?\\n");
+//				for(int i=0;i<newInfoArr.length;i++){
+//					String[] line = newInfoArr[i].split(":");
+//					if(line[0].equalsIgnoreCase("name")){
+//						updateChecklist[0]=1;
+//						newInfoContentOnly[0]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("type")){
+//						updateChecklist[1]=1;
+//						newInfoContentOnly[1]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("starttime")){
+//						updateChecklist[2]=1;
+//						newInfoContentOnly[2]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("endtime")){
+//						updateChecklist[3]=1;
+//						newInfoContentOnly[3]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("location")){
+//						updateChecklist[4]=1;
+//						newInfoContentOnly[4]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("note")){
+//						updateChecklist[5]=1;
+//						newInfoContentOnly[5]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("tag")){
+//						updateChecklist[6]=1;
+//						newInfoContentOnly[6]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("important")){
+//						updateChecklist[7]=1;
+//						newInfoContentOnly[7]= line[1];
+//					}
+//					else if(line[0].equalsIgnoreCase("finished")){
+//						updateChecklist[8]=1;
+//						newInfoContentOnly[8]= line[1];
+//					}
+//				
+//					
+//				}
+//				return new CommandUpdate(index, updateChecklist, newInfoContentOnly);
+//			}
+//			catch (NumberFormatException e){
+//				return new CommandInvalid(rawInput);
+//			}
+//		}
+//		
+//		return new CommandInvalid(rawInput);
+//	}
