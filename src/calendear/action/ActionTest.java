d5b7f975c359b5c8058578a88fc5a3fa02e20236
a0129998 <a0129998@u.nus.edu>
2016-03-20 23:07:29 +0800

    added basic test cases in ActionTest

--- src/calendear/action/ActionTest.java
+++ src/calendear/action/ActionTest.java
@@ -20,7 +20,7 @@ public class ActionTest {
 		CommandAdd cA = new CommandAdd(t1);
 		action1.exeAdd(cA);
 		GregorianCalendar newTime = new GregorianCalendar(2, 1, 2001);
-		System.out.println(nextIndex);
+		//System.out.println(nextIndex);
 		boolean[] chklst = {false, false, false, true, false, false, false, false, false};
 		Object[] objArr = {null, null, null, (Object) newTime, null, null, null, null};
 		CommandUpdate cU = new CommandUpdate(nextIndex, chklst, objArr);
@@ -30,14 +30,14 @@ public class ActionTest {
 	
 	@Test
 	public void testUndo() throws ParseException{
-		Action action1 = new Action("action1.txt");
+		Action action1 = new Action("action2.txt");
 		int nextIndex = action1.getAmount();
 		GregorianCalendar originalTime = new GregorianCalendar(01, 01, 2001);
 		Task t1 = new Task("task2", originalTime);
 		CommandAdd cA = new CommandAdd(t1);
 		action1.exeAdd(cA);
 		GregorianCalendar newTime = new GregorianCalendar(2, 1, 2001);
-		System.out.println(nextIndex);
+		//System.out.println(nextIndex);
 		boolean[] chklst = {false, false, false, true, false, false, false, false, false};
 		Object[] objArr = {null, null, null, (Object) newTime, null, null, null, null};
 		CommandUpdate cU = new CommandUpdate(nextIndex, chklst, objArr);
@@ -48,5 +48,25 @@ public class ActionTest {
 		assertEquals(t1.getEndTime(), originalTime);
 	}
 
-
+	@Test
+	public void testRedo() throws ParseException{
+		Action action1 = new Action("action3.txt");
+		int nextIndex = action1.getAmount();
+		GregorianCalendar originalTime = new GregorianCalendar(01, 01, 2001);
+		Task t1 = new Task("task2", originalTime);
+		CommandAdd cA = new CommandAdd(t1);
+		action1.exeAdd(cA);
+		GregorianCalendar newTime = new GregorianCalendar(2, 1, 2001);
+		//System.out.println(nextIndex);
+		boolean[] chklst = {false, false, false, true, false, false, false, false, false};
+		Object[] objArr = {null, null, null, (Object) newTime, null, null, null, null};
+		CommandUpdate cU = new CommandUpdate(nextIndex, chklst, objArr);
+		action1.exeUpdate(cU);
+		assertEquals(t1.getEndTime(), newTime);
+		
+		action1.exeUndo();
+		assertEquals(t1.getEndTime(), originalTime);
+		action1.exeRedo();
+		assertEquals(t1.getEndTime(), newTime);
+	}
 }
