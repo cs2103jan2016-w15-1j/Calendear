d656a96b3df18262ceda1f7d26aa5a55de6057a2
a0129998 <a0129998@u.nus.edu>
2016-03-27 18:08:30 +0800

    updated exeImportance and exeDone, updated functions relating to selective display, only display important/not important/done/not done

--- src/calendear/util/CommandMark.java
+++ src/calendear/util/CommandMark.java
@@ -3,14 +3,19 @@ package calendear.util;
 public class CommandMark extends Command {
 	
 	private int index;
+	private boolean isImportant;
 	
-	public CommandMark(int index){
+	public CommandMark(int index, boolean importance){
 		type = CMD_TYPE.MARK;
 		this.index = index;
+		this.isImportant = importance;
 	}
 	
 	public int getIndex(){
 		return index;
 	}
 	
+	public boolean isImportant(){
+		return this.isImportant;
+	}
 }
